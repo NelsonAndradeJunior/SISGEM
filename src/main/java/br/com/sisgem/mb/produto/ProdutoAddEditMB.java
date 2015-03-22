279683cc1d120939a037237188afab33b4810e11
NelsonAndradeJunior <nelsonpcinfo@gmail.com>
2015-03-22 17:36:17 -0300

    produtos

---
+++ src/main/java/br/com/sisgem/mb/produto/ProdutoAddEditMB.java
@@ -0,0 +1,69 @@
+package br.com.sisgem.mb.produto;
+
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import br.com.sisgem.model.ProdutoEntity;
+import br.com.sisgem.model.repository.IProdutoRepository;
+import br.com.sisgem.model.utils.BaseBeans;
+
+@Component
+@Scope ("view")
+@Named (value = "produtoAddEditMB")
+public class ProdutoAddEditMB extends BaseBeans{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private IProdutoRepository produtoRepository;
+	
+	@Inject
+	private FacesContext context;
+	
+	@Inject
+	private ProdutoMB mbProdutoBean;
+	
+	private ProdutoEntity produtoObj;
+	
+	public ProdutoAddEditMB() {
+		this.produtoObj = new ProdutoEntity();
+	}
+	
+	public void salvar(){
+		this.produtoRepository.save(this.produtoObj);
+	}
+	
+	public void update(){
+		this.produtoObj = mbProdutoBean.getProdutoSelecionado();
+	}
+	
+	////Getters and Setters////
+	public IProdutoRepository getProdutoRepository() {
+		return produtoRepository;
+	}
+
+	public void setProdutoRepository(IProdutoRepository produtoRepository) {
+		this.produtoRepository = produtoRepository;
+	}
+
+	public FacesContext getContext() {
+		return context;
+	}
+
+	public void setContext(FacesContext context) {
+		this.context = context;
+	}
+
+	public ProdutoEntity getProdutoObj() {
+		return produtoObj;
+	}
+
+	public void setProdutoObj(ProdutoEntity produtoObj) {
+		this.produtoObj = produtoObj;
+	}
+
+}
--- src/main/java/br/com/sisgem/model/ProdutoEntity.java
+++ src/main/java/br/com/sisgem/model/ProdutoEntity.java
@@ -16,19 +16,12 @@ public class ProdutoEntity extends BaseEntities<Long> {
 
 	private static final long serialVersionUID = 1L;
 	
-	private String Nome;
-	private String Unidade;
-	private Integer Valor;
-	public FornecedorEntity getFornecedor_idPessoaJuridica() {
-		return Fornecedor_idPessoaJuridica;
-	}
-
-	public void setFornecedor_idPessoaJuridica(
-			FornecedorEntity fornecedor_idPessoaJuridica) {
-		Fornecedor_idPessoaJuridica = fornecedor_idPessoaJuridica;
-	}
-
-	private String EPI;
+	private String nome;
+	private String unidade_medida;
+	private Integer valor_venda;
+	private Integer valor_compra;		
+	private String epi;
+	private String conteudo;
 	
 	@ManyToOne
 	@JoinColumn(name="Fornecedor_idPessoaJuridica")
@@ -38,49 +31,74 @@ public class ProdutoEntity extends BaseEntities<Long> {
 		
 	}
 
-	public ProdutoEntity(Integer fornecedor_idPessoaJuridica, String nome,
-			String unidade, Integer valor, String ePI) {
+	public ProdutoEntity(String nome, String unidade_medida,
+			Integer valor_venda, Integer valor_compra, String epi,
+			String conteudo) {
 		super();
-		
-		Nome = nome;
-		Unidade = unidade;
-		Valor = valor;
-		EPI = ePI;
+		this.nome = nome;
+		this.unidade_medida = unidade_medida;
+		this.valor_venda = valor_venda;
+		this.valor_compra = valor_compra;
+		this.epi = epi;
+		this.conteudo = conteudo;
 	}
 
-
 	public String getNome() {
-		return Nome;
+		return nome;
 	}
 
 	public void setNome(String nome) {
-		Nome = nome;
+		this.nome = nome;
 	}
 
-	public String getUnidade() {
-		return Unidade;
+	public String getUnidade_medida() {
+		return unidade_medida;
 	}
 
-	public void setUnidade(String unidade) {
-		Unidade = unidade;
+	public void setUnidade_medida(String unidade_medida) {
+		this.unidade_medida = unidade_medida;
 	}
 
-	public Integer getValor() {
-		return Valor;
+	public Integer getValor_venda() {
+		return valor_venda;
 	}
 
-	public void setValor(Integer valor) {
-		Valor = valor;
+	public void setValor_venda(Integer valor_venda) {
+		this.valor_venda = valor_venda;
 	}
 
-	public String getEPI() {
-		return EPI;
+	public Integer getValor_compra() {
+		return valor_compra;
 	}
 
-	public void setEPI(String ePI) {
-		EPI = ePI;
+	public void setValor_compra(Integer valor_compra) {
+		this.valor_compra = valor_compra;
+	}
+
+	public String getEpi() {
+		return epi;
+	}
+
+	public void setEpi(String epi) {
+		this.epi = epi;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public FornecedorEntity getFornecedor_idPessoaJuridica() {
+		return Fornecedor_idPessoaJuridica;
+	}
+
+	public void setFornecedor_idPessoaJuridica(
+			FornecedorEntity fornecedor_idPessoaJuridica) {
+		Fornecedor_idPessoaJuridica = fornecedor_idPessoaJuridica;
 	}
 	
 	
-
 }
